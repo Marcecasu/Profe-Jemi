c79ee85429f6bfb29b5e5b9dd92516d82434fc51
Marcecasu <marce.casu@gmail.com>
2026-03-26 22:05:08 -0300

    feat(branding): update app favicon and pwa icon

--- vite.config.ts
+++ vite.config.ts
@@ -18,12 +18,12 @@ export default defineConfig({
         start_url: '/?source=pwa',
         icons: [
           {
-            src: '/profe_jemi.png',
+            src: '/icon.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/profe_jemi.png',
+            src: '/icon.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
